01f003e558a5354b0f3c3b41e1b468114a8ea763
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-21 15:39:12 -0300

    atribuindo valores ao botão e mostrando em tela

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
-var numero = window.document.getElementById('numero')
-var num = Number(numero.value)
-var operacao = window.document.getElementById('operacao')
+const num1 = window.document.getElementById('num1');
+num1.addEventListener('click', atribuir1);
+const res = window.document.getElementById('res');
 
-
-function somar (){
-    const res = num + operacao + num
-    
-       resultado.innerHTML = res
-
+function atribuir1(){
+    const num1 = 1;
+    res.innerHTML = `${num1}`;   
 }
