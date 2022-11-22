ec799444e643978bcd3217bfd09a1af373367d5f
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-22 17:06:12 -0300

    ajuste css, tags html, e evento com insert de numero onclick

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-const num1 = window.document.getElementById('num1');
-num1.addEventListener('click', atribuir1);
-const res = window.document.getElementById('res');
 
-function atribuir1(){
-    const num1 = 1;
-    res.innerHTML = `${num1}`;   
+
+function insert(num){
+    const res = window.document.getElementById('resultado');
+    res.innerHTML = num;
 }
