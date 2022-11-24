7199c0fd5b7ba6f101da20bc971b835cfad50dcd
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-24 15:37:03 -0300

    funcionalidade backspace em js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,7 @@ function clean(){
     window.document.getElementById('resultado').innerHTML = "";
 }
     
+function apagar(){
+    var resultado = window.document.getElementById('resultado').innerHTML;
+    window.document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
