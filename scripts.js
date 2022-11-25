4437d7603eb946f4e039724fe56fc2e32681e01d
Mateus Pettenuzzo <mpnmateus@gmail.com>
2022-11-25 17:06:27 -0300

    inserida a última funçao eval que faz o cálculo no botão =

--- scripts.js
+++ scripts.js
@@ -12,4 +12,15 @@ function clean(){
 function apagar(){
     var resultado = window.document.getElementById('resultado').innerHTML;
     window.document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+}
+
+function calcular(){
+    var resultado = window.document.getElementById('resultado').innerHTML;
+    if(resultado) {
+        window.document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        window.document.getElementById('resultado').innerHTML = "";
+    }
 }
